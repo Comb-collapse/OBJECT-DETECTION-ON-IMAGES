298fe42c5eb8d4a356c3f408081be99d007d718d
Comb-collapse <104627240+Comb-collapse@users.noreply.github.com>
2023-10-22 18:14:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+function Bed() {
+  location.href = "bedroom.html";
+  return false;
+}
+function TV() {
+  location.href = "TV.html";
+  return false;
+}
+function Bottles() {
+  location.href = "Bottles.html";
+  return false;
+}
+function Desk() {
+  location.href = "Desk.html";
+  return false;
+}
+function fruity(){
+  location.href = "fruity.html";
+  return false;
+}
